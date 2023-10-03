d4daac70a8ad55301fd3e30a410c7c469eae7c49
Hex <hexmusictheory@protonmail.com>
2023-10-03 14:34:09 +0100

    update seed

--- src/database/seed.sql
+++ src/database/seed.sql
@@ -1,8 +1,8 @@
 BEGIN;
 
 INSERT INTO users VALUES
-  (1, 'steveTheWizard', '$2a$12$A74tKw96m82AEZpJrIEQxecZGscKayJD/hD5/I6DuqKEJoQlAlNYO', '2017-12-25 00:00:00', 'images/mage-1.jpg', 1, 0, 0, 'You haven''t added this yet'),
-  (2, 'sharon123', '$2a$12$o2NP8ykiki.T11qRQOCnBeX68PhwXHprM/hxHkbAfqotWo2XT.vdW', '2017-12-25 00:00:00', 'images/mage-2.jpg', 1, 0, 0, 'You haven''t added this yet'),
+  (1, 'steveTheWizard', '$2a$12$A74tKw96m82AEZpJrIEQxecZGscKayJD/hD5/I6DuqKEJoQlAlNYO', '2017-12-25 00:00:00', 'images/mage-1.jpg', 1, 1, 0, 'You haven''t added this yet'),
+  (2, 'sharon123', '$2a$12$o2NP8ykiki.T11qRQOCnBeX68PhwXHprM/hxHkbAfqotWo2XT.vdW', '2017-12-25 00:00:00', 'images/mage-2.jpg', 1, 0, 1, 'You haven''t added this yet'),
   (3, 'harrystylesABC', '$2a$12$8HIQ9kqN0brBUfDigfSfB.9BlSsICw4LQ/tiw4U/zMW5QWtuTqMG6', '2017-12-25 00:00:00', 'images/mage-3.jpg', 1, 0, 0, 'You haven''t added this yet'),
   (4, 'demonLover', '$2a$12$vpUb03L0WVHMfrHducL2SumogekzuPfh9Ych0Il8Zkt5Baqch7QZq', '2017-12-25 00:00:00', 'images/mage-4.jpg', 1, 0, 0, 'You haven''t added this yet'),
   (5, 'barbara', '$2a$12$4luxIrDKiU.bpasgzWuRwurBeGX8JNc7Q.taioE6nP3ZUGsN8cH2i', '2017-12-25 00:00:00', 'images/entity-1.jpg', 0, 0, 0, 'You haven''t added this yet'),
