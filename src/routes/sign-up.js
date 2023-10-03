f164d5dfd7fefb5827f29b80069eeaab1c859754
DeepsDali <deepashri.dali@gmail.com>
2023-10-03 11:11:46 +0100

    Add sign-up page

--- src/routes/sign-up.js
+++ src/routes/sign-up.js
@@ -1,6 +1,7 @@
-const { createUser } = require("../model/user.js");
-const { createSession } = require("../model/session.js");
+// const { createUser } = require("../model/user.js");
+// const { createSession } = require("../model/session.js");
 const { Layout } = require("../templates.js");
-const bcrypt = require("bcryptjs");
-
+// const bcrypt = require("bcryptjs");
+const signup = require("./routes/sign-up.js");
+server.get("/sign-up", signup.get);
 module.exports = { get, post };
