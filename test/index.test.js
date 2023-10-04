0cf2e031e1f4b605ca5daf6880ae7c57e056d528
tess-phillips <tess.phillips@me.com>
2023-10-04 13:08:04 +0100

    conflicts removed

--- test/index.test.js
+++
@@ -1,7 +0,0 @@
-const test = require("node:test");
-const assert = require("node:assert");
-const { resetDatabase } = require("../src/database/reset.js");
-
-test("testing is working", () => {
-    resetDatabase();
-});
