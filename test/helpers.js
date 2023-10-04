e557473822c3b2793a8480b07af4353a00e9e4fd
tess-phillips <tess.phillips@me.com>
2023-10-04 14:25:08 +0100

    adding sign-up routes test successfully

--- test/helpers.js
+++ test/helpers.js
@@ -1,4 +1,5 @@
 const db = require(`../src/database/db.js`);
+const server = require(`../src/server.js`);
 
 function reset() {
     db.exec(/*sql*/ `
@@ -20,9 +21,29 @@ function getLastId(tableName) {
     return result.last_id;
   }
 
-  module.exports = {
+async function request(pathname, options = {}) {
+    const app = server.listen(0);
+    const { port } = app.address();
+    const url = new URL(pathname, `http://localhost:${port}`);
+    options.headers = { ...options.headers, connection: "close" };
+    const response = await fetch(url, options);
+    app.close();
+    const body = await response.text();
+    const headers = Object.fromEntries(response.headers);
+    return { status: response.status, body, headers };
+}
+  
+function get_sid(headers) {
+    const [sid_cookie] = headers["set-cookie"].split(".");
+    const encoded_sid = sid_cookie.replace("sid=s%3A", "");
+    return decodeURIComponent(encoded_sid);
+}
+
+module.exports = {
     reset,
     db,
     count,
-    getLastId
+    getLastId,
+    request,
+    get_sid
   };
