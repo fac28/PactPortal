b76de2ee61080e091b8968c8a762c2aa1e43a712
Hex <hexmusictheory@protonmail.com>
2023-10-05 11:59:30 +0100

    finished all formatting / text content / graphics

--- src/templates/home.js
+++ src/templates/home.js
@@ -25,7 +25,7 @@ function home() {
         </div>
         <div class="card">
             <div class="card-caption">
-                <h4>Esotericism for Everyone</h3>
+                <h4>Heretically Efficient</h3>
                 <p>By automating the most repetitive aspects of ritual magick behind the scenes, we help you focus on what's important: the infernal pact between a sorcerer and an astral entity.<p>
             </div>
             <img src="images/pentagram.png" />
@@ -39,11 +39,15 @@ function home() {
         </div>
         <div class="card">
             <div class="card-caption">
-                <h4>Seals. Simplified.</h3>
-                <p>QR codes provide an easier, more secure way to register pacts, making complicated seals a thing of the past.<p>
+                <h4>Dark Arts, Bright Future</h3>
+                <p>Embrace the future of astral communication! Our modern platform combines the ancient with cutting-edge technology, bringing the supernatural world into the 21st century.<p>
             </div>
-            <img src="images/qr-code.png" />
+            <img src="images/mobile.png" />
         </div>
+    </div>
+    <div class="section-2">
+        <h2>Esotericism is for everyone.</h2>
+        <p class="blurb">Welcome to PactPortal, where the mystical meets the digital age! Uniting wizards and demons in an enchanting fusion of occult ceremony and modern technology, we've conjured up the ultimate platform for seekers of arcane connections. Create your profile, cast your charm, and let the sparks of infernal pacts fly in a world where heresy, arcane power, and communication converge. Swipe, chat, and unlock the secrets of the supernatural like never before—because in the realm of magic, even the most otherworldly connections start with a simple click.</p>
     </div>
 
     `;
