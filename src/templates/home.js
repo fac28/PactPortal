c1c89c3f46385b4ebd3fc8ae73cfab6fb196487b
Hex <hexmusictheory@protonmail.com>
2023-10-05 08:19:48 +0100

    completed seed data, added more html/css filler content

--- src/templates/home.js
+++ src/templates/home.js
@@ -16,7 +16,20 @@ function home(req, res) {
     </div>
     <h2>We're disrupting sorcery.</h2>
     <div class="cards">
-        <div class="card"></div>
+        <div class="card">
+            <div class="card-caption">
+                <h4>Seals. Simplified.</h3>
+                <p>QR codes provide an easier, more secure way to register pacts, making complicated seals a thing of the past.<p>
+            </div>
+            <img src="images/qr-code.png" />
+        </div>
+        <div class="card">
+            <div class="card-caption">
+                <h4>Esotericism for Everyone</h3>
+                <p>By automating the most repetitive aspects of ritual magick behind the scenes, we help you focus on what's important: the infernal pact between a sorcerer and an astral entity.<p>
+            </div>
+            <img src="images/pentagram.png" />
+        </div>
     </div>
 
     `;
@@ -30,11 +43,18 @@ function users(req, res, isWizard) {
     const users = getUserList(isWizard ? 0 : 1);
     const content = /*html*/ `
     <div class="banner">
-    <div>
-        <a href="/user"><button class="button">Profile</button></a>
-        <a href="/log-out"><button class="button">Log Out</button></a>
+        <div class="title">
+            <img src="images/logo.svg" /><h1>PactPortal</h1>
+        </div>
+        <p class="subtitle">Bringing wizards and demons together</p>
     </div>
-        ${users.map(user => createPost(user))}
+    <div class="match-container">
+        <div>
+            <a href="/user"><button class="button">Profile</button></a>
+            <a href="/log-out"><button class="button">Log Out</button></a>
+        </div>
+        <h2>${isWizard ? 'Demons' : 'Wizards'} Near You</h2>
+        ${users.map(user => createPost(user)).join('')}
     </div>
 `
     return layout({ title, content });
@@ -42,12 +62,11 @@ function users(req, res, isWizard) {
 
 function createPost(user) {
     return /*html*/ `
-    
     <div class="post">
         <p class="post-username">${user.username}</p>
         <img src="${user.imageURL}" class="post-picture" />
         <p class="post-bio">${user.bio}</p>
-        <button class="button"> Impose Ban </button>
+        <button class="button">Banish</button>
     </div>
 `
 }
